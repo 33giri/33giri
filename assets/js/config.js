359504a75001aa503ce8b11a309ed290d0b3bdf3
33Catalogo <33giristudios@gmail.com>
2026-02-22 02:56:09 +0100

    Update adminCode in config.js

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+// assets/js/config.js
+window.APP_CONFIG = {
+  appName: "33Giri Catalogo",
+  heroTitle: "Arte in Vinile",
+  heroSubtitle: "Vinili trasformati in pezzi unici. Ogni pezzo racconta una storia musicale.",
+  whatsappNumber: "393000000000", // <-- cambia qui
+  adminCode: "Ciaocia0!",             // <-- cambia qui
+  storageKey: "33giri_catalog_v1"
+};
